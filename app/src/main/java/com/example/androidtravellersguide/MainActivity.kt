daaeb6ee769047bed4edc7db22a051bbd982a56a
unknown <allanmathenge2.com>
2024-03-01 21:34:41 +0300

    added functionality

--- app/src/main/java/com/example/androidtravellersguide/MainActivity.kt
+++ app/src/main/java/com/example/androidtravellersguide/MainActivity.kt
@@ -3,11 +3,12 @@ package com.example.androidtravellersguide
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    var currentImage = 0
-
+    private var currentImage = 0
+    lateinit var image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,16 +18,39 @@ class MainActivity : AppCompatActivity() {
 
         next.setOnClickListener{
             // Next Image is scrolled to view
-            var idCurrentImageString = "duck$currentImage"
+            var idCurrentImageString = "silt$currentImage"
 
             // Get integer address associated with each view
+            var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString, "id", packageName)
 
+            image = findViewById(idCurrentImageInt)
+            image.alpha = 0f
+
+            currentImage = (5+currentImage+1)%5
+            var idImageToShowString = "silt"+currentImage
+            var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
+
+            image = findViewById(idImageToShowInt)
+            image.alpha = 1f
         }
 
         prev.setOnClickListener{
             // Prev image is scrolled to view
 
-        }
+            var idCurrentImageString = "silt$currentImage"
 
+            // Get integer address associated with each view
+            var idCurrentImageInt = this.resources.getIdentifier(idCurrentImageString, "id", packageName)
+
+            image = findViewById(idCurrentImageInt)
+            image.alpha = 0f
+
+            currentImage = (5+currentImage-1)%5
+            var idImageToShowString = "silt"+currentImage
+            var idImageToShowInt = this.resources.getIdentifier(idImageToShowString, "id", packageName)
+
+            image = findViewById(idImageToShowInt)
+            image.alpha = 1f
+        }
     }
 }
